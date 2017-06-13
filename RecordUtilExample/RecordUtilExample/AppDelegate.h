167928137cf8a7efa86d0081e3dad9d50ba2582b
PlutoMa <mademaomail@126.com>
2017-06-13 16:19:36 +0800

    Create iOS Project

---
+++ RecordUtilExample/RecordUtilExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecordUtilExample
+//
+//  Created by Dareway on 2017/6/13.
+//  Copyright © 2017年 Pluto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
